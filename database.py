5168ac0584907733a1bfb8730a6bdc7317749034
parthmalhotra-tech <parth07092007malhotra@gmail.com>
2026-07-13 23:48:13 +0530

    postgresql

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 import os
 from sqlalchemy.orm import declarative_base,sessionmaker
 from dotenv import load_dotenv
+load_dotenv()
 database_url=os.getenv("DATABASE_URL",'sqlite:///movie_explorer_database.db')
 
 engine=create_engine(database_url)
